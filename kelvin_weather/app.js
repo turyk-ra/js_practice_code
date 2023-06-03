17a477fd895d07d2afaadf40b4af3b578cd43909
rat_93 <roman.turik48@gmail.com>
2023-06-03 16:02:16 +0300

    extra newton

--- kelvin_weather/app.js
+++ kelvin_weather/app.js
@@ -1,9 +1,16 @@
 //temperature in kelvin
 const kelvin = 293;
+//log kelvin temperature
+console.log(`The temperature is ${kelvin} degrees Kelvin.`)
 //temperature in celsius
 let celcius = kelvin - 273;
+//log celcius temperatue
+console.log(`The temperature is ${celcius} degrees Celcius.`)
 //convert and round temperature in fahrenheit from celsius
 let fahrenheit = Math.floor(celcius * (9/5) + 32);
 //log the F temerature
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
-//done
+//convert and round celcius to the Newton
+let newton = Math.floor(celcius * (33/100));
+//log newton temperature
+console.log(`The temperature is ${newton} degrees Newton.`)
