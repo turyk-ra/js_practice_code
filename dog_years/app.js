5c6fac56ad0766635f1eac0d7bddcea15fe933b1
rat_93 <roman.turik48@gmail.com>
2023-06-03 16:20:07 +0300

    dog age practice

---
+++ dog_years/app.js
@@ -0,0 +1,12 @@
+//myAge const variable
+const myAge = 30;
+// this variable convert two human years in first years of dog
+let earlyYears = 2 * 10.5;
+//convert other human years in dog years
+let laterYears = (myAge - 2) * 4;
+// here we count sum of years
+let myAgeInDogYears = earlyYears + laterYears;
+// name variable to logging
+const myName = "Raman".toLowerCase();
+console.log(`My name is ${myName}. 
+I'm ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
