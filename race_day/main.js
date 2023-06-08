a3df76938615c84676474c47b50c669968b78d53
rat_93 <roman.turik48@gmail.com>
2023-06-08 07:28:16 +0300

    commit

--- race_day/main.js
+++ race_day/main.js
@@ -5,14 +5,14 @@ let registerEarly = true;
 // define a variable that indicates runners age
 let runnerAge = 20;
 // control flow - checks age and reg time to determine race time + check raceNumber
-if ( registerEarly && runnerAge > 18){
+if (registerEarly && runnerAge > 18) {
     raceNumber += 1000;
     console.log('Early adults run at 9:30 am');
-}else if( !registerEarly && runnerAge > 18){
+} else if (!registerEarly && runnerAge > 18) {
     console.log('Late adults run at 11:00 am');
-}else if( runnerAge < 18){
+} else if (runnerAge < 18) {
     console.log('Youth registrants run at 12:30 pm (regardless of registration)');
-}else{
+} else {
     console.log('Watch the registration desk.');
 }
 console.log(`Your race number is ${raceNumber}`);
