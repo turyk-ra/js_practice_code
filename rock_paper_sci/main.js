c620809d07a1254696f2a4eb4e3af88d24dee36f
rat_93 <roman.turik48@gmail.com>
2023-06-10 04:11:14 +0300

    rock/paper/scissors

---
+++ rock_paper_sci/main.js
@@ -0,0 +1,68 @@
+//create a func that takes user choice
+const getUserChoice = userInput => {
+    //the user can write variable in different capitalization
+    userInput = userInput.toLowerCase();
+    //validation check for user input
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return userInput;
+    }else if(userInput === 'bomb'){
+        return userInput;
+    }else {
+        console.log('invalid value');
+    }
+};
+//create a func that takes computer choice
+const getComputerChoice = () => {
+    let computerChoice = ''
+    let computerRandomChoice = Math.floor(Math.random() * 3);
+    if (computerRandomChoice === 0) {
+        computerChoice = 'rock';
+    } else if (computerRandomChoice === 1) {
+        computerChoice = 'paper';
+    } else if (computerRandomChoice === 2) {
+        computerChoice = 'scissors';
+    } else {
+        console.log('invalid value');
+    }
+    return computerChoice;
+};
+//create a func to determine who's a winner
+const determineWinner = (userInput, computerChoice) => {
+    if (userInput === computerChoice) {
+        return 'tie';
+    }
+    if(userInput === 'bomb'){
+        return 'You absolutely won this game';
+    }
+    if (userInput === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'Computer won';
+        } else {
+            return 'You won';
+        }
+    }
+    if (userInput === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'Computer won';
+        } else {
+            return 'You won';
+        }
+    }
+    if (userInput === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'Computer won';
+        } else {
+            return 'You won';
+        }
+    }
+}
+
+function playGame() {
+    let userChoice = getUserChoice('rock');
+    let computerChoice = getComputerChoice();
+    console.log(userChoice);
+    console.log(computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
