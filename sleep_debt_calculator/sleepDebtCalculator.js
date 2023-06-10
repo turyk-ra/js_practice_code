2a5604d0a088c4f5617cc2fa08a6d49ed6ba5db5
rat_93 <roman.turik48@gmail.com>
2023-06-10 17:30:49 +0300

    reformat

--- sleep_debt_calculator/sleepDebtCalculator.js
+++ sleep_debt_calculator/sleepDebtCalculator.js
@@ -1,45 +1,39 @@
 //create a function that determine how many hours of sleep do you got each night of the week
 const getSleepHours = day => {
-    //variable for hours
-    let sleepingHours = 0;
     //every day result of hours
     switch (day) {
         case 'monday':
-            sleepingHours += 8;
+            let sleepingHours = 8;
             break;
         case 'tuesday':
-            sleepingHours += 7;
+            sleepingHours = 7;
             break;
         case 'wednesday':
-            sleepingHours += 7;
+            sleepingHours = 7;
             break;
         case 'thursday':
-            sleepingHours += 6;
+            sleepingHours = 6;
             break;
         case 'friday':
-            sleepingHours += 4;
+            sleepingHours = 4;
             break;
         case 'saturday':
-            sleepingHours += 8;
+            sleepingHours = 8;
             break;
         case 'sunday':
-            sleepingHours += 8;
+            sleepingHours = 8;
             break;
     }
     return sleepingHours;
 };
 //create a function to get total sleep hours with implicit return
-const getActualSleepHours = () => getSleepHours('Monday') + getSleepHours('tuesday') + getSleepHours('wednesday')
-    + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday');
+const getActualSleepHours = () => 8 + 7 + 7 + 6 + 4 + 8 + 8;
 //create a function to get total ideal hours of sleeping per week
-const getIdealSleepHours = () => {
-    let idealHours = 8;
-    return idealHours * 7;
-};
+const getIdealSleepHours = idealHours => idealHours * 7;
 //create a function to calculate sleep debt
 const calculateSleepDebt = () => {
     let actualSleepHours = getActualSleepHours();
-    let idealSleepHours = getIdealSleepHours();
+    let idealSleepHours = getIdealSleepHours(8);
     if (actualSleepHours === idealSleepHours) {
         console.log(`the user sleep ${actualSleepHours} this week, ideal hours - ${idealSleepHours}`);
         console.log('the user got the perfect amount of sleep.');
