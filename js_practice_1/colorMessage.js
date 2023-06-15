6bafb7db5206519d8e3d7c712dc4e84195046e4b
rat_93 <roman.turik48@gmail.com>
2023-06-15 21:04:57 +0300

    first practice

---
+++ js_practice_1/colorMessage.js
@@ -0,0 +1,5 @@
+// Create a function colorMessage() that takes 2 string arguments, favoriteColor and shirtColor.
+// If the value of favoriteColor is the same as the value of shirtColor return the string 'The shirt is your favorite color!'.
+// If not, return the string 'That is a nice color.'
+
+const colorMessage = (favoriteColor, shirtColor) => favoriteColor === shirtColor ? 'The shirt is your favorite color!' : 'That is a nice color.'
---
+++ js_practice_1/isEven.js
@@ -0,0 +1,3 @@
+// Create a function isEven() that takes a number as its only parameter. 
+// The function should return true if the number is even and false if the number is odd.
+const isEven = number => number % 2 === 0 ? true : false
