0b00fd09be95e8e223076f4449ec7f7a0901aff7
unknown <roman.turik48@gmail.com>
2024-06-10 21:01:23 +0300

    practice code

--- JavaScript Practice-Arrays, Loops, Objects, Iterators/groceries.js
+++ JavaScript Practice-Arrays, Loops, Objects, Iterators/groceries.js
@@ -1,13 +1,17 @@
 // Write a function groceries() that takes an array of object literals of grocery items. The function should return a string with each item separated by a comma except the last two items should be separated by the word 'and'. Make sure spaces (' ') are inserted where they are appropriate.
 
-const groceries = array => {
-    let newArr = [];
-    array.forEach(element => {
-        newArr.push(element.item);
-    })
-    let str = newArr.join(',');
-    console.log(str);
-}
+const groceries = list => {
+    let listString = '';
+    for (let i = 0; i < list.length; i++) {
+        listString += list[i].item;
+        if (i < list.length - 2) {
+            listString += ', ';
+        } else if (i === list.length - 2) {
+            listString += ' and ';
+        }
+    }
+    return listString;
+};
 
 
 
